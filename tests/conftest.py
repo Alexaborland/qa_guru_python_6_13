c0da5409da17d26ded3a4e76c9e583e063dd1a98
Aleksandra Kashina <Borland3711@gmail.com>
2023-09-15 23:58:50 +0400

    changed conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import os
 import pytest
-from selenium.webdriver.chrome.options import Options
-from dotenv import load_dotenv
-from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Config, browser
+from dotenv import load_dotenv
 from utils import attach
 
 DEFAULT_BROWSER_VERSION = "100.0"
@@ -15,11 +15,6 @@ def pytest_addoption(parser):
         default='100.0'
     )
 
-    parser.addoption(
-        '--browser',
-        default="chrome"
-    )
-
 
 @pytest.fixture(scope='session', autouse=True)
 def load_env():
@@ -27,17 +22,14 @@ def load_env():
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_open(request):
+def setup_browser(request):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.timeout = 10
 
     browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version \
-        if browser_version != "" \
-        else DEFAULT_BROWSER_VERSION
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
+
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": browser_version,
@@ -46,10 +38,11 @@ def browser_open(request):
             "enableVideo": True
         }
     }
-    options.capabilities.update(selenoid_capabilities)
 
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
+
+    options.capabilities.update(selenoid_capabilities)
 
     driver = webdriver.Remote(
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
